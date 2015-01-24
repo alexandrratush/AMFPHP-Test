19ac5905d95021dc0ccf8bbadfea9719dbc1052b
alexandrratush <alexandrratush@gmail.com>
2015-01-24 20:20:22 +0200

    Change amfphp entry point

--- server/index.php
+++ server/index.php
@@ -1,12 +1,14 @@
 <?php
 
-require_once dirname(__FILE__) . '/vendor/autoload.php';
+define('ROOT', dirname(__FILE__) . "/lib/Amfphp/");
+
+require_once ROOT . "/ClassLoader.php";
 
 $serviceFolders = dirname(__FILE__) . '/src/';
 $voFolders = array($serviceFolders . '/vo/');
 
 $config = new Amfphp_Core_Config();
-$config->serviceFolderPaths = array($serviceFolders);
+$config->serviceFolders = array($serviceFolders);
 $config->pluginsConfig['AmfphpVoConverter'] = array('voFolders' => $voFolders);
 
 $gateway = Amfphp_Core_HttpRequestGatewayFactory::createGateway($config);
